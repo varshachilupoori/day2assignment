595682d15308d8c321cf7590ddd130c73413d119
varshachilupoori <88825938+varshachilupoori@users.noreply.github.com>
2021-08-17 14:20:58 +0530

    Add files via upload

---
+++ Sumofnumbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Sumofnumbers
+{
+	public static void main(String[] args)
+	{
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a value");
+		a=sc.nextInt();
+		System.out.println("enter b value");
+		b=sc.nextInt();
+		c=a+b;
+		System.out.println("sum of two numbers is"+c);
+	}
+}
+		
